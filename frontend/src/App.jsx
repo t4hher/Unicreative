6175a59a20878c3f5e274bee207199a1aa033d62
lahmadiilyas1@gmail.com <lahmadiilyas1-tech>
2026-04-06 19:42:34 +0200

    ajouter La page Candidature et la page Realisation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,15 @@ import Accueil from './Components/Accuiel'
 import Apropos from './Components/Apropos'
 import Login from './Components/Login'
 import Footer from './Components/Footer'
+import Condidature from './Components/Condidature'
+import Realisation from './Components/Realisation'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (<div className=''>
     <Navabar/>
-        <Apropos/>
+        <Realisation/>
     <Footer/>
   </div>)
 }
---
+++ frontend/src/Components/Realisation.jsx
@@ -0,0 +1,34 @@
+export default function Realisation(){
+    return(
+        <div>
+            <div className="realisation">
+                <h1 className="titrerealisation">Nos Réalisation</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur  reprehenderit accusamus?</p>
+            </div>
+            <div className="lienesf">
+                <div className="lienfilteres">
+                    <a href="#">Tous</a>
+                    <a href="#">Filter1</a>
+                    <a href="#">Filter2</a>
+                    <a href="#">Filter3</a>
+                </div> 
+            </div>
+            <div className="imgrealisation">
+                <img src="/Realisation/i14.jpg" alt="" />
+                <img src="/Realisation/i15.jpg" alt="" />
+                <img src="/Realisation/i16.jpg" alt="" />
+                <img src="/Realisation/i17.jpg" alt="" />
+                <img src="/Realisation/i18.jpg" alt="" />
+                <img src="/Realisation/i19.jpg" alt="" />
+                <img src="/Realisation/i20.jpg" alt="" />
+                <img src="/Realisation/i21.jpg" alt="" />
+                <img src="/Realisation/i22.jpg" alt="" />
+                <img src="/Realisation/i23.jpg" alt="" />
+                <img src="/Realisation/i24.jpg" alt="" />
+                <img src="/Realisation/i25.jpg" alt="" />
+                <img src="/Realisation/i26.jpg" alt="" />
+                <img src="/Realisation/i27.jpg" alt="" />
+            </div>
+        </div>
+    )
+}
